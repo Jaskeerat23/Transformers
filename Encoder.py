f6e2e0e2bfe298bf9f07d70bd87a63c6d79f6a5f
Jaskeerat Singh <jaskeerat23006@gmail.com>
2025-12-23 16:18:38 +0530

    Fixed Issues

--- Encoder.py
+++ Encoder.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
 
 class Encoder(nn.Module):
     
-    def __init__(self, num_heads, d_model, d_ff, device, mask = None, post_norm = True):
+    def __init__(self, num_heads, d_model, d_ff, device, post_norm = True):
         super().__init__()
         self.post_norm = post_norm
         self.num_heads = num_heads
@@ -24,7 +24,7 @@ class Encoder(nn.Module):
         self.layer_norm2 = nn.LayerNorm(d_model)
         
         #Multi Head Attention Part
-        self.self_attn = Attention.MultiHeadAttention(d_model = d_model, num_heads = self.num_heads, device = device, mask = mask)
+        self.self_attn = Attention.MultiHeadAttention(d_model = d_model, num_heads = self.num_heads, device = device)
         
         #The below projection layers are used to transform X to Q, K, V respectively
         self.w_q_enc = nn.Linear(in_features = d_model, out_features = d_model)
@@ -35,18 +35,18 @@ class Encoder(nn.Module):
         self.dropout_1 = nn.Dropout(p = 0.1)
         self.dropout_2 = nn.Dropout(p = 0.1)
     
-    def forward(self, X):
+    def forward(self, X, mask = None):
         
         Q = self.w_q_enc(X)
         K = self.w_k_enc(X)
         V = self.w_v_enc(X)
         
         if __name__ == "__main__":
-            print(f"The shape of Q after dividing heads is {Q.shape}")
-            print(f"The shape of K after dividing heads is {K.shape}")
-            print(f"The shape of V after dividing heads is {V.shape}")
+            print(f"The shape of Q is {Q.shape}")
+            print(f"The shape of K is {K.shape}")
+            print(f"The shape of V is {V.shape}")
         
-        multi_attn_out = self.self_attn(Q, K, V)
+        multi_attn_out = self.self_attn(Q, K, V, mask)
         
         sub_layer_1 = self.layer_norm1(X + self.dropout_1(multi_attn_out))
         
